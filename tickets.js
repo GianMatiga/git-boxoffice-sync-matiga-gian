67d43b99f2acbe2162a66cc66844626e7ef015cf
GianMatiga1 <gian.matiga@cit.edu>
2026-09-23 21:16:20 +0800

    Add 50% VIP surcharge for premium seating - matiga.gian

--- tickets.js
+++ tickets.js
@@ -2,8 +2,14 @@ function isValidQuantity(quantity) {
   return quantity > 0 && quantity <= 20;
 }
 
-function calculateTicketPrice(quantity, basePrice) {
-  return Math.floor(quantity * basePrice);
+function calculateTicketPrice(quantity, basePrice, premium) {
+  let price = quantity * basePrice;
+
+  if (premium) {
+    price = price * 1.5;
+  }
+
+  return Math.floor(price);
 }
 
 module.exports = { isValidQuantity, calculateTicketPrice };
